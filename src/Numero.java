366bd16b2da335a975ec02dea934ea40eedaf364
Melvin González <54681730+melagora@users.noreply.github.com>
2025-04-29 17:38:56 -0600

    clase simple que encapsula un número real (double)

---
+++ src/Numero.java
@@ -0,0 +1,20 @@
+/**
+ * La clase Numero representa un número con un valor de tipo double.
+ * Proporciona métodos para obtener y establecer el valor del número.
+ */
+
+public class Numero {
+    private double valor;
+
+    public Numero(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
