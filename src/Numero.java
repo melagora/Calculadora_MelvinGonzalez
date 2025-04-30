58b5129c1d8e912f4a152e423823a99d5215b624
Melvin González <54681730+melagora@users.noreply.github.com>
2025-04-30 00:34:38 -0600

    Documentación para la clase Numero

--- src/Numero.java
+++ src/Numero.java
@@ -1,19 +1,31 @@
 /**
- * La clase Numero representa un número con un valor de tipo double.
- * Proporciona métodos para obtener y establecer el valor del número.
+ * La clase Numero representa un número real utilizando un valor de tipo double.
+ * Se utiliza para encapsular el valor numérico en objetos dentro de operaciones
+ * matemáticas.
  */
 
 public class Numero {
+
+    // Atributo que almacena el valor del número
     private double valor;
 
+    /**
+     * Constructor que inicializa el número con un valor específico.
+     */
     public Numero(double valor) {
         this.valor = valor;
     }
 
+    /**
+     * Método para obtener el valor del número.
+     */
     public double getValor() {
         return valor;
     }
 
+    /**
+     * Método para establecer un nuevo valor al número.
+     */
     public void setValor(double valor) {
         this.valor = valor;
     }
